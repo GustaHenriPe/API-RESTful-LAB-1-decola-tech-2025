2a3035c5a568798d304b801d0a1dd70b5dc7d45d
gustavo <guhhpj@gmail.com>
2025-03-24 12:31:41 -0300

    fix: cors

--- src/main/java/com/dio/decola_tech_2025/Application.java
+++ src/main/java/com/dio/decola_tech_2025/Application.java
@@ -1,9 +1,11 @@
 package com.dio.decola_tech_2025;
 
+import com.dio.decola_tech_2025.domain.OpenApiConfig;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@OpenAPIDefinition
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/dio/decola_tech_2025/domain/OpenApiConfig.java
+++ src/main/java/com/dio/decola_tech_2025/domain/OpenApiConfig.java
@@ -3,14 +3,11 @@ package com.dio.decola_tech_2025.domain;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class CorsCOnfig {
+public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().addServersItem(new Server().url("Sua nova URL"))
                 .addServersItem(new Server().url("http://localhost:8080"));
     }
-
 }
