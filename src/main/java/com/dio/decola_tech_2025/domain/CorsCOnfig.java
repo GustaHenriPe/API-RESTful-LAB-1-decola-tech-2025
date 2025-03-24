cf6ceb8e24cb348792b0e12490ada3af352e0526
gustavo <guhhpj@gmail.com>
2025-03-24 12:22:05 -0300

    fix: cors

--- src/main/java/com/dio/decola_tech_2025/domain/CorsCOnfig.java
+++ src/main/java/com/dio/decola_tech_2025/domain/CorsCOnfig.java
@@ -1,24 +1,16 @@
 package com.dio.decola_tech_2025.domain;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsCOnfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("Sua nova URL"))
+                .addServersItem(new Server().url("http://localhost:8080"));
     }
 
 }
